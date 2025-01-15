96ffd37b7c92b404733c43a19932acf3504c17b2
H4ymak3r <mirko.damonte@hotmail.com>
2025-01-15 10:09:37 +0100

    Aggiungere i file di progetto.

---
+++ Greeting Kata/GreetingKata.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Greeting_Kata
+{
+    internal class GreetingKata
+    {
+        public string greet(string name)
+        {
+            if (name == null) {
+                return "Hello, my friend.";
+            }
+            else { 
+                return $"Hello, {name}";
+            }
+            if (name == name.ToUpper())
+            {
+                return $"HELLO, {name.ToUpper()}";
+            }
+        }
+    }
+}
